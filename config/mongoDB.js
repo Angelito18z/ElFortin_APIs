aa29ab4d7516d9e8bd70d2f229e6d0ee630d2d8e
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-03 16:47:28 -0600

    fix: usar variable de entorno para conexion a mongo

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -6,7 +6,7 @@ dotenv.config();
 // MONGO_URI=mongodb://<username>:<password>@<host>:<port>/<database>
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/PanaderiaElFortin'); // No need for the deprecated options anymore
+        await mongoose.connect(process.env.MONGO_URI); // No need for the deprecated options anymore
         console.log('✅ MongoDB connected successfully');
     } catch (error) {
         console.error('❌ MongoDB connection error:', error);
